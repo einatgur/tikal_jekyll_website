192b8add0f59b438daefc99c3db3fa5bb737cda0
Assaf G <assaf@tikalk.com>
2014-11-15 18:41:43 +0200

    integrated facebook widget

--- _assets/javascripts/events.js
+++ _assets/javascripts/events.js
@@ -3,7 +3,7 @@
 	'use strict';
 
 	var Sn = {};
-	var module = angular.module('meetups-module', []);
+	var module = angular.module('events-module', []);
 
 	Sn.MeetupsApiParams = {
 		baseApiUrl: 'https://api.meetup.com',
@@ -45,7 +45,7 @@
 
 			getMeetups: function(status, offset, page, desc){
 				var defer = $q.defer(),
-				    params = getJSONPParams(status, offset, page, desc);
+					params = getJSONPParams(status, offset, page, desc);
 
 
 				function onSuccess(data){
@@ -68,13 +68,13 @@
 			},
 			getMeetupRsvps: function(meetupId){
 				var defer = $q.defer(),
-				    apiUrl =
-					    [MeetupsApiParams.baseApiUrl, '2', 'rsvps'].join('/'),
-				    params = {
-					    callback: MeetupsApiParams.callback,
-					    key: MeetupsApiParams.key,
-					    event_id: meetupId
-				    };
+					apiUrl =
+						[MeetupsApiParams.baseApiUrl, '2', 'rsvps'].join('/'),
+					params = {
+						callback: MeetupsApiParams.callback,
+						key: MeetupsApiParams.key,
+						event_id: meetupId
+					};
 
 
 				function onSuccess(data){
@@ -92,18 +92,18 @@
 			},
 			getMeetupAttendance: function(meetupId){
 				var defer = $q.defer(),
-				    apiUrl = [
-					    MeetupsApiParams.baseApiUrl,
-					    MeetupsApiParams.group_urlname,
-					    'events',
-					    meetupId,
-					    'attendance'
-				    ].join('/'),
-				    params = {
-					    callback: MeetupsApiParams.callback,
-					    key: MeetupsApiParams.key,
-					    sign: MeetupsApiParams.sign
-				    };
+					apiUrl = [
+						MeetupsApiParams.baseApiUrl,
+						MeetupsApiParams.group_urlname,
+						'events',
+						meetupId,
+						'attendance'
+					].join('/'),
+					params = {
+						callback: MeetupsApiParams.callback,
+						key: MeetupsApiParams.key,
+						sign: MeetupsApiParams.sign
+					};
 
 
 				function onSuccess(data){
@@ -147,15 +147,15 @@
 	module.controller('Sn.MeetupsCtrl', ['$scope', 'MeetupsService', function($scope, MeetupsService){
 
 		var _self = this,
-		    lastOffset = 0;
+			lastOffset = 0;
 
 		function onUpComingSuccess(meetups){
 			_self.meetupsByStatus.upcoming = meetups;
 			var i = 0,
-			    len = meetups.length;
+				len = meetups.length;
 
 			for(; i < len; ++i){
-console.log("meetups[i] = ", meetups[i]);
+				console.log("meetups[i] = ", meetups[i]);
 				MeetupsService.getMeetupRsvps(meetups[i].id).then(function(data){
 					if (data.length > 0) {
 						_self.meetupsRsvps[data[0].event.id] = data;
@@ -166,7 +166,7 @@ console.log("meetups[i] = ", meetups[i]);
 
 		function onPastSuccess(meetups){
 			var i = 0,
-			    len = meetups.length;
+				len = meetups.length;
 
 
 			for(; i < len; ++i){
@@ -188,9 +188,9 @@ console.log("meetups[i] = ", meetups[i]);
 			MeetupsService
 				.getMeetups('past', offset, 3, 'true')
 				.then(
-					onPastSuccess,
-					onError
-				);
+				onPastSuccess,
+				onError
+			);
 		}
 
 		_self.loadMore = function loadMore(){
@@ -208,9 +208,9 @@ console.log("meetups[i] = ", meetups[i]);
 			MeetupsService
 				.getMeetups('upcoming', 0, 20)
 				.then(
-					onUpComingSuccess,
-					onError
-				);
+				onUpComingSuccess,
+				onError
+			);
 
 			loadPastEvents(lastOffset);
 
@@ -233,9 +233,9 @@ console.log("meetups[i] = ", meetups[i]);
 			},
 			link: function (scope, element) {
 				var waitlistText = 'Waiting List',
-				    rsvpText = 'RSVP',
-				    waitlistClass = 'event-waitlist',
-				    rsvpClass = 'event-rsvp';
+					rsvpText = 'RSVP',
+					waitlistClass = 'event-waitlist',
+					rsvpClass = 'event-rsvp';
 
 				scope.isWaitlist = function (meetup) {
 					return meetup.yes_rsvp_count >= meetup.rsvp_limit || meetup.waitlist_count;
@@ -268,5 +268,95 @@ console.log("meetups[i] = ", meetups[i]);
 			}
 		};
 	}]);
+
+	module.service('FBDataService', ['$http', '$window', '$q', function($http, $window, $q){
+		var fb = $window.FB;
+
+		fb.init({
+			appId: '334506690062624',
+			status: true,
+			cookie: true,
+			xfbml: true
+		});
+
+		this.getData = function () {
+			var deferred = $q.defer();
+			fb.api(
+				"/v2.2/225585444263260/feed?access_token=334506690062624|wYA-QpptpE0UZBjBTicmS2JKkIU",
+				function (response) {
+					if(response.error){
+						deferred.reject(response.error);
+					} else if (response && !response.error) {
+						deferred.resolve(response);
+					}
+				});
+
+			return deferred.promise;
+
+		}
+
+	}]);
+
+	module.controller('FeedCtrl', ['$scope', 'FBDataService', function($scope, FBDataService){
+		$scope.data = [];
+
+		FBDataService.getData().then(function(res){
+			var feeds = res.data.slice(0,5);
+			for (var i=0; i<feeds.length; i++){
+				var id = feeds[i].id;
+				var feedId = [];
+				feedId.push(id.substring(0,id.indexOf("_")));
+				feedId.push(id.substring(id.indexOf("_")+1));
+
+				var feed = {
+					image: "https://graph.facebook.com/" + feeds[i].from.id + "/picture",
+					authorName: feeds[i].from.name,
+					profilePage: "https://www.facebook.com/app_scoped_user_id/" + feeds[i].from.id,
+					created: feeds[i].created_time,
+					message: feeds[i].message,
+					postMessage: "https://www.facebook.com/" + feedId[0] + "/posts/" + feedId[1]
+				}
+
+				$scope.data.push(feed);
+			}
+
+		},function(reason){
+			console.log(reason);
+		});
+	}]);
+
+
+	module.filter('dateSuffix', function($filter) {
+		var suffixes = ["th", "st", "nd", "rd"];
+		return function(input) {
+			var dtfilter = $filter('date')(input, 'MMM, dd');
+			var day = parseInt(dtfilter.slice(-2));
+			var relevantDigits = (day < 30) ? day % 20 : day % 30;
+			var suffix = (relevantDigits <= 3) ? suffixes[relevantDigits] : suffixes[0];
+			return dtfilter+suffix;
+		};
+	});
+
+
+	module.filter('cut', function () {
+		return function (value, wordwise, max, tail) {
+			if (!value) return '';
+
+			max = parseInt(max, 10);
+			if (!max) return value;
+			if (value.length <= max) return value;
+
+			value = value.substr(0, max);
+			if (wordwise) {
+				var lastspace = value.lastIndexOf(' ');
+				if (lastspace != -1) {
+					value = value.substr(0, lastspace);
+				}
+			}
+
+			return value + (tail || ' …');
+		};
+	});
+
 
 })();
